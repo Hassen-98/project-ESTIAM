6d005ab77c66257ef3d7b96f85bbc8711483c174
Salah <salah78300@gmail.com>
2021-11-01 15:20:51 +0100

    Data Done

---
+++ my-app/src/data/meats.data.ts
@@ -0,0 +1,19 @@
+export interface Meat {
+  name: string;
+  slug: string;
+  image: string;
+}
+
+const meats: Meat[] = [
+  {
+    name: "Viande",
+    slug: "viande",
+    image: `${process.env.PUBLIC_URL}/assets/meats/viande.png`,
+  },
+  {
+    name: "Tofu",
+    slug: "tofu",
+    image: `${process.env.PUBLIC_URL}/assets/meats/tofu.png`,
+  },
+];
+export default meats;
