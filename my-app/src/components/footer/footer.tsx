6b3ad9dbe0b64d69c0ca16295504ab151848550c
hassen <hassen.agoun@isticbc.org>
2021-11-01 22:46:27 +0100

    test1

--- my-app/src/components/footer/footer.tsx
+++ my-app/src/components/footer/footer.tsx
@@ -8,7 +8,13 @@ interface Props {
 const Footer = (props: Props) => {
   return (
     <Styled.FooterWrapper>
-      <h1>Magic Kebab</h1>
+     <div>
+      <h1>Fun Fact </h1>
+      <p> Le goût et l'odeur de l'agneau auraient été jugés trop forts par le
+        client occidental qui préfère des viandes au caractère gustatif moins
+        fort. On retrouve donc aujourd'hui quasi exclusivement du veau et de la
+        dinde dans nos sandwichs.</p>
+        </div>
     </Styled.FooterWrapper>
   )
 }
