087787d6f9544970d17c09083eb39a107da389ab
Salah <salah78300@gmail.com>
2021-11-02 13:35:12 +0100

    fix error

--- my-app/src/components/footer/footer.tsx
+++ my-app/src/components/footer/footer.tsx
@@ -1,22 +1,9 @@
-import React from 'react'
-import * as Styled from './footer.styled';
-
-interface Props {
-  
-}
-
+import React from "react";
+import * as Styled from "./footer.styled";
+interface Props {}
 const Footer = (props: Props) => {
   return (
     <Styled.FooterWrapper>
-<<<<<<< HEAD
-     <div>
-      <h1>Fun Fact </h1>
-      <p> Le goût et l'odeur de l'agneau auraient été jugés trop forts par le
-        client occidental qui préfère des viandes au caractère gustatif moins
-        fort. On retrouve donc aujourd'hui quasi exclusivement du veau et de la
-        dinde dans nos sandwichs.</p>
-        </div>
-=======
       <h2>Fun Fact</h2>
       <p>
         Le goût et l'odeur de l'agneau auraient été jugés trop forts par le
@@ -24,9 +11,7 @@ const Footer = (props: Props) => {
         fort. On retrouve donc aujourd'hui quasi exclusivement du veau et de la
         dinde dans nos sandwichs.
       </p>
->>>>>>> main
     </Styled.FooterWrapper>
   );
-}
-
-export default Footer
+};
+export default Footer;
